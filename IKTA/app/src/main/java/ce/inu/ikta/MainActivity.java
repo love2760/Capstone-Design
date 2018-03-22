94e3e27aaedb85f92899fbe73834ff3a0b2b1e24
wo-hu <l.syad.r@gmail.com>
2018-03-22 14:25:01 +0900

    카메라 구현만 완료

--- IKTA/app/src/main/java/ce/inu/ikta/MainActivity.java
+++ IKTA/app/src/main/java/ce/inu/ikta/MainActivity.java
@@ -1,36 +1,481 @@
 package ce.inu.ikta;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
+        import android.Manifest;
+        import android.annotation.TargetApi;
+        import android.app.Activity;
+        import android.app.AlarmManager;
+        import android.app.PendingIntent;
+        import android.content.Context;
+        import android.content.DialogInterface;
+        import android.content.Intent;
+        import android.content.pm.PackageManager;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.graphics.Matrix;
+        import android.hardware.Camera;
+        import android.hardware.Camera.PictureCallback;
+        import android.hardware.Camera.ShutterCallback;
+        import android.net.Uri;
+        import android.os.AsyncTask;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.os.Environment;
+        import android.provider.Settings;
+        import android.support.annotation.NonNull;
+        import android.support.v4.app.ActivityCompat;
+        import android.support.v4.content.ContextCompat;
+        import android.support.v7.app.AlertDialog;
+        import android.support.v7.app.AppCompatActivity;
+        import android.util.Log;
+        import android.view.Surface;
+        import android.view.SurfaceView;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.view.ViewGroup.LayoutParams;
+        import android.view.Window;
+        import android.view.WindowManager;
+        import android.widget.Button;
+        import android.widget.FrameLayout;
+        import android.widget.Toast;
 
-/* 주석을 잘 달도록 하자
-/* 날짜 이름 내용을 적어주자
- * 인자가 뭘 의미하는지 잘 말해주도록 하자.
- */
+        import java.io.ByteArrayOutputStream;
+        import java.io.File;
+        import java.io.FileNotFoundException;
+        import java.io.FileOutputStream;
+        import java.io.IOException;
+
+
+
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    Preview preview;
+    Camera camera;
+    Context ctx;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+    private final static int PERMISSIONS_REQUEST_CODE = 100;
+    // Camera.CameraInfo.CAMERA_FACING_FRONT or Camera.CameraInfo.CAMERA_FACING_BACK
+    private final static int CAMERA_FACING = Camera.CameraInfo.CAMERA_FACING_BACK;
+    private AppCompatActivity mActivity;
+
+
+    public static void doRestart(Context c) {
+        //http://stackoverflow.com/a/22345538
+        try {
+            //check if the context is given
+            if (c != null) {
+                //fetch the packagemanager so we can get the default launch activity
+                // (you can replace this intent with any other activity if you want
+                PackageManager pm = c.getPackageManager();
+                //check if we got the PackageManager
+                if (pm != null) {
+                    //create the intent with the default start activity for your application
+                    Intent mStartActivity = pm.getLaunchIntentForPackage(
+                            c.getPackageName()
+                    );
+                    if (mStartActivity != null) {
+                        mStartActivity.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP );
+                        //create a pending intent so the application is restarted
+                        // after System.exit(0) was called.
+                        // We use an AlarmManager to call this intent in 100ms
+                        int mPendingIntentId = 223344;
+                        PendingIntent mPendingIntent = PendingIntent
+                                .getActivity( c, mPendingIntentId, mStartActivity,
+                                        PendingIntent.FLAG_CANCEL_CURRENT );
+                        AlarmManager mgr =
+                                (AlarmManager) c.getSystemService( Context.ALARM_SERVICE );
+                        mgr.set( AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent );
+                        //kill the application
+                        System.exit( 0 );
+                    } else {
+                        Log.e( TAG, "Was not able to restart application, " +
+                                "mStartActivity null" );
+                    }
+                } else {
+                    Log.e( TAG, "Was not able to restart application, PM null" );
+                }
+            } else {
+                Log.e( TAG, "Was not able to restart application, Context null" );
+            }
+        } catch (Exception ex) {
+            Log.e( TAG, "Was not able to restart application" );
+        }
     }
 
-    TessOCR mtess;
+    public void startCamera() {
+
+        if (preview == null) {
+            preview = new Preview( this, (SurfaceView) findViewById( R.id.surfaceView ) );
+            preview.setLayoutParams( new LayoutParams( LayoutParams.MATCH_PARENT,
+                    LayoutParams.MATCH_PARENT ) );
+            ((FrameLayout) findViewById( R.id.layout )).addView( preview );
+            preview.setKeepScreenOn( true );
+
+            /* 프리뷰 화면 눌렀을 때  사진을 찍음
+            preview.setOnClickListener(new OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+                    camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+                }
+            });*/
+        }
+
+        preview.setCamera( null );
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+
+        int numCams = Camera.getNumberOfCameras();
+        if (numCams > 0) {
+            try {
+
+                camera = Camera.open( CAMERA_FACING );
+                // camera orientation
+                camera.setDisplayOrientation( setCameraDisplayOrientation( this, CAMERA_FACING,
+                        camera ) );
+                // get Camera parameters
+                Camera.Parameters params = camera.getParameters();
+                // picture image orientation
+                params.setRotation( setCameraDisplayOrientation( this, CAMERA_FACING, camera ) );
+                camera.startPreview();
+
+            } catch (RuntimeException ex) {
+                Toast.makeText( ctx, "camera_not_found " + ex.getMessage().toString(),
+                        Toast.LENGTH_LONG ).show();
+                Log.d( TAG, "camera_not_found " + ex.getMessage().toString() );
+            }
+        }
+
+        preview.setCamera( camera );
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        ctx = this;
+        mActivity = this;
 
-        mtess = new TessOCR("path","lang");
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //상태바 없애기
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN );
+
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+
+        setContentView( R.layout.activity_main );
+
+        Button button = (Button) findViewById( R.id.button_capture );
+        button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                camera.takePicture( shutterCallback, rawCallback, jpegCallback );
+            }
+        } );
+
+
+        if (getPackageManager().hasSystemFeature( PackageManager.FEATURE_CAMERA )) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { //API 23 이상이면
+                // 런타임 퍼미션 처리 필요
+
+                int hasCameraPermission = ContextCompat.checkSelfPermission( this,
+                        Manifest.permission.CAMERA );
+                int hasWriteExternalStoragePermission =
+                        ContextCompat.checkSelfPermission( this,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE );
+
+                if (hasCameraPermission == PackageManager.PERMISSION_GRANTED
+                        && hasWriteExternalStoragePermission == PackageManager.PERMISSION_GRANTED) {
+                    ;//이미 퍼미션을 가지고 있음
+                } else {
+                    //퍼미션 요청
+                    ActivityCompat.requestPermissions( this,
+                            new String[]{Manifest.permission.CAMERA,
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            PERMISSIONS_REQUEST_CODE );
+                }
+            } else {
+                ;
+            }
+
+
+        } else {
+            Toast.makeText( MainActivity.this, "Camera not supported",
+                    Toast.LENGTH_LONG ).show();
+        }
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Surface will be destroyed when we return, so stop the preview.
+        if (camera != null) {
+            // Call stopPreview() to stop updating the preview surface
+            camera.stopPreview();
+            preview.setCamera( null );
+            camera.release();
+            camera = null;
+        }
+
+        ((FrameLayout) findViewById( R.id.layout )).removeView( preview );
+        preview = null;
+
+    }
+
+    private void resetCam() {
+        startCamera();
+    }
+
+    private void refreshGallery(File file) {
+        Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE );
+        mediaScanIntent.setData( Uri.fromFile( file ) );
+        sendBroadcast( mediaScanIntent );
+    }
+
+    ShutterCallback shutterCallback = new ShutterCallback() {
+        public void onShutter() {
+            Log.d( TAG, "onShutter'd" );
+        }
+    };
+
+    PictureCallback rawCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Log.d( TAG, "onPictureTaken - raw" );
+        }
+    };
+
+
+    //참고 : http://stackoverflow.com/q/37135675
+    PictureCallback jpegCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            //이미지의 너비와 높이 결정
+            int w = camera.getParameters().getPictureSize().width;
+            int h = camera.getParameters().getPictureSize().height;
+
+            int orientation = setCameraDisplayOrientation( MainActivity.this,
+                    CAMERA_FACING, camera );
+
+            //byte array를 bitmap으로 변환
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeByteArray( data, 0, data.length, options );
+            //int w = bitmap.getWidth();
+            //int h = bitmap.getHeight();
+
+            //이미지를 디바이스 방향으로 회전
+            Matrix matrix = new Matrix();
+            matrix.postRotate( orientation );
+            bitmap = Bitmap.createBitmap( bitmap, 0, 0, w, h, matrix, true );
+
+            //bitmap을 byte array로 변환
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress( Bitmap.CompressFormat.JPEG, 100, stream );
+            byte[] currentData = stream.toByteArray();
+
+            //파일로 저장
+            new SaveImageTask().execute( currentData );
+            resetCam();
+            Log.d( TAG, "onPictureTaken - jpeg" );
+        }
+    };
+
+    private class SaveImageTask extends AsyncTask<byte[], Void, Void> {
+
+        @Override
+        protected Void doInBackground(byte[]... data) {
+            FileOutputStream outStream = null;
+
+            // Write to SD Card
+            try {
+                File sdCard = Environment.getExternalStorageDirectory();
+                File dir = new File( sdCard.getAbsolutePath() + "/camtest" );
+                dir.mkdirs();
+
+                String fileName = String.format( "%d.jpg", System.currentTimeMillis() );
+                File outFile = new File( dir, fileName );
+
+                outStream = new FileOutputStream( outFile );
+                outStream.write( data[0] );
+                outStream.flush();
+                outStream.close();
+
+                Log.d( TAG, "onPictureTaken - wrote bytes: " + data.length + " to "
+                        + outFile.getAbsolutePath() );
+
+                refreshGallery( outFile );
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+            }
+            return null;
+        }
+
     }
 
     /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
+     * @param activity
+     * @param cameraId Camera.CameraInfo.CAMERA_FACING_FRONT,
+     *                 Camera.CameraInfo.CAMERA_FACING_BACK
+     * @param camera   Camera Orientation
+     *                 reference by https://developer.android.com/reference/android/hardware/Camera.html
      */
-    public native String stringFromJNI();
+    public static int setCameraDisplayOrientation(Activity activity,
+                                                  int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo( cameraId, info );
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+
+        return result;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grandResults) {
+
+        if (requestCode == PERMISSIONS_REQUEST_CODE && grandResults.length > 0) {
+
+            int hasCameraPermission = ContextCompat.checkSelfPermission( this,
+                    Manifest.permission.CAMERA );
+            int hasWriteExternalStoragePermission =
+                    ContextCompat.checkSelfPermission( this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE );
+
+            if (hasCameraPermission == PackageManager.PERMISSION_GRANTED
+                    && hasWriteExternalStoragePermission == PackageManager.PERMISSION_GRANTED) {
+
+                //이미 퍼미션을 가지고 있음
+                doRestart( this );
+            } else {
+                checkPermissions();
+            }
+        }
+
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkPermissions() {
+        int hasCameraPermission = ContextCompat.checkSelfPermission( this,
+                Manifest.permission.CAMERA );
+        int hasWriteExternalStoragePermission =
+                ContextCompat.checkSelfPermission( this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE );
+
+        boolean cameraRationale = ActivityCompat.shouldShowRequestPermissionRationale( this,
+                Manifest.permission.CAMERA );
+        boolean writeExternalStorageRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale( this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE );
+
+
+        if ((hasCameraPermission == PackageManager.PERMISSION_DENIED && cameraRationale)
+                || (hasWriteExternalStoragePermission == PackageManager.PERMISSION_DENIED
+                && writeExternalStorageRationale))
+            showDialogForPermission( "앱을 실행하려면 퍼미션을 허가하셔야합니다." );
+
+        else if ((hasCameraPermission == PackageManager.PERMISSION_DENIED && !cameraRationale)
+                || (hasWriteExternalStoragePermission == PackageManager.PERMISSION_DENIED
+                && !writeExternalStorageRationale))
+            showDialogForPermissionSetting( "퍼미션 거부 + Don't ask again(다시 묻지 않음) " +
+                    "체크 박스를 설정한 경우로 설정에서 퍼미션 허가해야합니다." );
+
+        else if (hasCameraPermission == PackageManager.PERMISSION_GRANTED
+                || hasWriteExternalStoragePermission == PackageManager.PERMISSION_GRANTED) {
+            doRestart( this );
+        }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void showDialogForPermission(String msg) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this );
+        builder.setTitle( "알림" );
+        builder.setMessage( msg );
+        builder.setCancelable( false );
+        builder.setPositiveButton( "예", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //퍼미션 요청
+                ActivityCompat.requestPermissions( MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        PERMISSIONS_REQUEST_CODE );
+            }
+        } );
+
+        builder.setNegativeButton( "아니오", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        } );
+        builder.create().show();
+    }
+
+    private void showDialogForPermissionSetting(String msg) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this );
+        builder.setTitle( "알림" );
+        builder.setMessage( msg );
+        builder.setCancelable( true );
+        builder.setPositiveButton( "예", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+                Intent myAppSettings = new Intent( Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.parse( "package:" + mActivity.getPackageName() ) );
+                myAppSettings.addCategory( Intent.CATEGORY_DEFAULT );
+                myAppSettings.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
+                mActivity.startActivity( myAppSettings );
+            }
+        } );
+        builder.setNegativeButton( "아니오", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        } );
+        builder.create().show();
+    }
 }
