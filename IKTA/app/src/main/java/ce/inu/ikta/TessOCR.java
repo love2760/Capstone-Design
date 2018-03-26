4004ae81e95883a47be06be170ce4fa6d5ce4172
광현 <kh030728@gmail.com>
2018-03-27 00:41:42 +0900

    안드로이드 테서렉트 코드 수정

--- IKTA/app/src/main/java/ce/inu/ikta/TessOCR.java
+++ IKTA/app/src/main/java/ce/inu/ikta/TessOCR.java
@@ -1,5 +1,6 @@
 package ce.inu.ikta;
 
+        import android.content.Context;
         import android.graphics.Bitmap;
 
         import com.googlecode.tesseract.android.TessBaseAPI;
@@ -10,9 +11,25 @@ package ce.inu.ikta;
 
 public class TessOCR {
     TessBaseAPI mtess;
-
-    TessOCR(String path, String lang) {
+    Context mCtx)
+    TessOCR(Context context) {
+        mCtx = context;
+        TessDataManager.initTessTrainedData(mCtx);
         mtess = new TessBaseAPI();
-        //mtess.init(path, lang);
+        String path = TessDataManager.getTrainedDataPath();
+        mtess.init(path,"ikta");
+        //추천 글자 설정
+        // mtess.setVariable(TessBaseAPI.VAR_CHAR_WHITELIST, "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        // 비추천 글자 설정
+        //mtess.setVariable(TessBaseAPI.VAR_CHAR_BLACKLIST, "!@#$%^&*()_+=-[]}{;:'\"\\|~`,./<>?");
+        mtess.setPageSegMode(TessBaseAPI.OEM_TESSERACT_CUBE_COMBINED);
+
+
+    }
+
+    public String requestOCR () {
+        //mtess.setImage(byte[] imagedata, int width, int height, int bpp, int bpl);
+        String output = mtess.getUTF8Text();
+        return output;
     }
 }
